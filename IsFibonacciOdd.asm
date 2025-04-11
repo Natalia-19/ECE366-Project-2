57adb04436c0bdadd957592b0938d23d3e7554bd
slawr3 <156039374+slawr3@users.noreply.github.com>
2025-04-10 22:28:48 -0500

    Update IsFibonacciOdd.asm

--- IsFibonacciOdd.asm
+++ IsFibonacciOdd.asm
@@ -1,18 +1,30 @@
 #.text 
 	.globl IsFibonacciOdd
 IsFibonacciOdd:
+    addi $sp, $sp, -8       # Allocate stack space
+    sw $ra, 4($sp)          # Save return address
+    sw $a0, 0($sp)          # Save original n
 
-    addi $sp, $sp, -8     # Allocate stack space
-    sw $ra, 4($sp)        # Save return address
-    sw $a0, 0($sp)        # Save original n
+    j call_Fibonacci        #Call Fibonacci(n)
 
-    j Fibonacci         # Call Fibonacci(n)
-    move $a0, $v0         # Move result of Fibonacci(n) to $a0 for Odd
+after_fibonacci:
+    move $a0, $v0           # Move result  of Fibonacci(n) into $a0 for Odd
 
-    j Odd               # Call Odd(Fib(n)), result in $v0
+    j call_Odd              # Call Odd(Fib(n)), result in $v0
 
-    lw $a0, 0($sp)        # Restore original $a0
-    lw $ra, 4($sp)        # Restore return address
-    addi $sp, $sp, 8      # Restore stack
-    jr $ra
-    end:
+after_odd:
+    lw $a0, 0($sp)          # Restore original n
+    lw $ra, 4($sp)          # Restore return address
+    addi $sp, $sp, 8        # Restore stack space
+    jr $ra                  # Return
+
+#function call with jump to save the return adress
+call_fibonacci:
+    jal Fibonacci
+    j after_fibonacci
+
+call_odd:
+    jal Odd
+    j after_odd
+
+end:
